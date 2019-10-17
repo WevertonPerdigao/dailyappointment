5f6a106da3dcde27a5ba983e1b33d7e707cb5b75
weverton.perdigao <wevertonperdigao@gmail.com>
2019-10-17 19:46:12 -0400

    aula-retrofit

---
+++ app/src/main/java/com/e/retrofit/services/Api.kt
@@ -0,0 +1,14 @@
+package com.e.retrofit.services
+
+import retrofit2.Call
+
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface Api {
+    @GET("badges")
+    fun getBadges(@QueryMap params: Map<String, String>): Call<Any>
+
+    @GET("comments")
+    fun getComments(@QueryMap params: Map<String, String>): Call<Any>
+}
