ec12eaa611c968f78d4dd927f0912370a8ea3e8a
weverton.perdigao <wevertonperdigao@gmail.com>
2019-10-30 15:59:21 -0400

    implementacao de metodos com viewModel

--- app/src/main/java/com/e/revisaoapp/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/e/revisaoapp/viewmodels/HomeViewModel.kt
@@ -1,5 +1,8 @@
 package com.e.revisaoapp.viewmodels
 
+import android.text.Editable
+import android.util.Log
+import android.view.View
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import com.e.revisaoapp.models.User
@@ -7,6 +10,24 @@ import com.e.revisaoapp.models.User
 class HomeViewModel : ViewModel() {
 
     var usuario = ObservableField<User>()
+
+
+    fun afterChanged(s: Editable) {
+        Log.d("afterChanged", "chamando afterChanged" + s)
+
+    }
+
+    fun beforeChanged(s: CharSequence, star: Int, count: Int, after: Int) {
+        Log.d("beforeChanged", "chamando beforeChanged" + s)
+    }
+
+    fun onChanged(s: CharSequence, star: Int, before: Int, count: Int) {
+        Log.d("onChanged", "chamando onChanged" + s)
+    }
+
+    fun buttonClick(view: View?) {
+        Log.d("buttonClick", "chamando buttonClick" + view)
+    }
 
 
 }
