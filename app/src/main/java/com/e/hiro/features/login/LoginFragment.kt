19823dea835116de9fbe936be560a48ee64da4e5
weverton.perdigao <wevertonperdigao@gmail.com>
2019-11-04 14:09:59 -0400

    tets

---
+++ app/src/main/java/com/e/hiro/features/login/LoginFragment.kt
@@ -0,0 +1,36 @@
+package com.e.hiro.features.login
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.e.hiro.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btn_login.setOnClickListener({
+            findNavController().navigate(R.id.home_graph)
+        })
+
+
+    }
+
+}
